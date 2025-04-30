1446f5d489fe3ea36aeab05249010f3f5168c587
mayur26062001 <mayurj@pre-scient.com>
2025-04-30 14:27:53 +0530

    Update runtest.bat

--- script/runtest.bat
+++ script/runtest.bat
@@ -1,15 +1,17 @@
 @echo off
 echo Running JUnit tests...
 
-:: Set the path for the JUnit JAR and other necessary libraries (if required)
-set JUNIT_JAR="C:\path\to\junit-4.13.2.jar"
-set HAMCREST_JAR="C:\path\to\hamcrest-core-1.3.jar"  :: If using Hamcrest with JUnit
+REM Set classpath to compiled classes + JUnit and Hamcrest JARs
+set CLASSPATH=.\bin;.\lib\junit-4.13.2.jar;.\lib\hamcrest-core-1.3.jar
 
-:: Set the classpath (include your compiled classes and test classes here)
-set CLASSPATH=..\bin;%JUNIT_JAR%;%HAMCREST_JAR%
+REM Run the test using JUnitCore
+java org.junit.runner.JUnitCore A.ProgramTest
 
-:: Run JUnit tests using the JUnit jar
-java -cp %CLASSPATH% org.junit.runner.JUnitCore A.ProgramTest
+REM Check error level and give feedback
+if %ERRORLEVEL% EQU 0 (
+    echo Tests ran successfully.
+) else (
+    echo Some tests failed.
+)
 
-:: Exit the script
-exit /b
+exit /b %ERRORLEVEL%
