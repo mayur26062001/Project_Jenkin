3939d5ad741edeeedca089c1d0bffc931b130c89
mayur26062001 <mayurj@pre-scient.com>
2025-04-30 15:32:59 +0530

    Update compileAndBuild.bat

--- script/compileAndBuild.bat
+++ script/compileAndBuild.bat
@@ -1,15 +1,15 @@
 @echo off
 echo Compiling Java files...
 
-:: Create output directory if it doesn't exist
-mkdir ..\bin 2>nul
+:: Create bin folder if not exists
+if not exist ..\bin mkdir ..\bin
 
-:: Compile Java files, with classpath pointing to JUnit and Hamcrest JARs
-javac -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar" -d ..\bin ..\src\A\Program.java ..\src\A\ProgramTest.java
+:: Compile main class
+javac --release 17 -d ..\bin ..\src\A\Program.java
 
-IF %ERRORLEVEL% NEQ 0 (
-    echo Compilation failed!
-    exit /b 1
-) ELSE (
-    echo Compilation successful!
-)
+:: Compile test class
+javac --release 17 -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar;..\bin" -d ..\bin ..\src\A\ProgramTest.java
+
+:: Run JUnit test
+echo Running tests...
+java -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar;..\bin" org.junit.runner.JUnitCore A.ProgramTest
