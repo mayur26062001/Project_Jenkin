681c47ed47f2b856f6028fdd7edccb5193ea5443
mayur26062001 <mayurj@pre-scient.com>
2025-05-05 17:13:54 +0530

    Update compileAndBuild.bat

--- script/compileAndBuild.bat
+++ script/compileAndBuild.bat
@@ -7,9 +7,7 @@ if not exist ..\bin mkdir ..\bin
 :: Compile main class
 javac --release 17 -d ..\bin ..\src\A\Program.java
 
-:: Compile test class
+:: Compile test class (without running the tests)
 javac --release 17 -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar;..\bin" -d ..\bin ..\src\A\ProgramTest.java
 
-:: Run JUnit test
-echo Running tests...
-java -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar;..\bin" org.junit.runner.JUnitCore A.ProgramTest
+echo Compilation finished.
