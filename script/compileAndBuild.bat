50f3e1591255d243ab40e34c25a24c23a0c7001c
mayur26062001 <mayurj@pre-scient.com>
2025-05-06 10:39:40 +0530

    Update compileAndBuild.bat

--- script/compileAndBuild.bat
+++ script/compileAndBuild.bat
@@ -1,16 +1,17 @@
 @echo off
 echo Compiling Java files...
 
-:: Set JAVA_HOME to JDK 17 if it's not already set
-if "%JAVA_HOME%"=="" set JAVA_HOME=C:\Program Files\Zulu\zulu-17
+:: Ensure JAVA_HOME is set to JDK 17
+if "%JAVA_HOME%"=="" (
+    set JAVA_HOME=C:\Program Files\Zulu\zulu-17
+)
 
-:: Create bin folder if not exists
-if not exist ..\bin mkdir ..\bin
+:: Clean the bin directory before compiling
+if exist ..\bin rmdir /s /q ..\bin
+mkdir ..\bin
 
-:: Compile main class
+:: Compile the classes using JDK 17
 "%JAVA_HOME%\bin\javac" --release 17 -d ..\bin ..\src\A\Program.java
-
-:: Compile test class
 "%JAVA_HOME%\bin\javac" --release 17 -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar;..\bin" -d ..\bin ..\src\A\ProgramTest.java
 
 echo Compilation finished.
