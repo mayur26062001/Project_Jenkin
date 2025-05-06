78e82308a96363ba2fc9ddbebda6cc7d939c08c5
mayur26062001 <mayurj@pre-scient.com>
2025-05-06 10:10:20 +0530

    Update compileAndBuild.bat

--- script/compileAndBuild.bat
+++ script/compileAndBuild.bat
@@ -1,16 +1,19 @@
 @echo off
 echo Compiling Java files...
 
-:: Set JDK 17 path (adjust the path to where your JDK 17 is installed)
-set JAVA_HOME="C:\Program Files\Java\jdk-17"
+:: Check JAVA_HOME is set
+if "%JAVA_HOME%"=="" (
+    echo JAVA_HOME is not set. Exiting.
+    exit /b 1
+)
 
 :: Create bin folder if not exists
 if not exist ..\bin mkdir ..\bin
 
-:: Compile main class using JDK 17
+:: Compile main class
 "%JAVA_HOME%\bin\javac" --release 17 -d ..\bin ..\src\A\Program.java
 
-:: Compile test class using JDK 17 (without running the tests)
+:: Compile test class
 "%JAVA_HOME%\bin\javac" --release 17 -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar;..\bin" -d ..\bin ..\src\A\ProgramTest.java
 
 echo Compilation finished.
