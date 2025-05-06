128e6b17813af16cddde28ced427460c04213baa
mayur26062001 <mayurj@pre-scient.com>
2025-05-06 10:40:11 +0530

    Update test.bat

--- script/test.bat
+++ script/test.bat
@@ -4,5 +4,5 @@ echo Running JUnit tests...
 :: Set JAVA_HOME to JDK 17 if it's not already set
 if "%JAVA_HOME%"=="" set JAVA_HOME=C:\Program Files\Zulu\zulu-17
 
-:: Run JUnit test
+:: Run JUnit test using JDK 17
 "%JAVA_HOME%\bin\java" -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-core-1.3.jar;..\bin" org.junit.runner.JUnitCore A.ProgramTest
